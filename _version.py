c821c61d08791710a2ff426ddab3f7ed07589c66
markspan <m.m.span@gmail.com>
2025-01-30 16:31:37 +0100

    Z

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev6+g9bbb3d7.d20250130'
-__version_tuple__ = version_tuple = (0, 1, 'dev6', 'g9bbb3d7.d20250130')
+__version__ = version = '0.1.dev7+g00c2be2.d20250130'
+__version_tuple__ = version_tuple = (0, 1, 'dev7', 'g00c2be2.d20250130')
